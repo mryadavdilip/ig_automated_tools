ade8db07e464cd872e8a6be87243e33634abe7bd
mryadavdilip <mryadavdilip@gmail.com>
2025-03-07 20:54:20 +0530

    first

---
+++ android/app/src/main/kotlin/com/example/ig_automated_tools/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ig_automated_tools
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,46 @@
-void main() {}
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:receive_sharing_intent/receive_sharing_intent.dart' as rsi;
+
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    rsi.ReceiveSharingIntent.instance.getMediaStream().listen(
+      _receiveData,
+      onError: (e) {
+        if (kDebugMode) {
+          print(e);
+        }
+      },
+    );
+    rsi.ReceiveSharingIntent.instance.getInitialMedia().then((value) {
+      _receiveData(value);
+      rsi.ReceiveSharingIntent.instance.reset();
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+
+  _receiveData(List<rsi.SharedMediaFile> value) {
+    for (var file in value) {
+      if (kDebugMode) {
+        print(file.path);
+      }
+    }
+  }
+}
