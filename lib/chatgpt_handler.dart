4902c7f4e2fee26661dd67cc12eb721968b7c836
mryadavdilip <mryadavdilip@gmail.com>
2025-03-09 22:58:32 +0530

    gpt

--- lib/chatgpt_handler.dart
+++ lib/chatgpt_handler.dart
@@ -4,33 +4,47 @@ import 'package:ig_automated_tools/hive_handler.dart';
 class ChatgptHandler {
   Future<OpenAI> get openAI async => OpenAI.instance.build(
     token: (await HiveHandler.getOpenAIKey()).key,
-    baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 5)),
+    baseOption: HttpSetup(
+      receiveTimeout: const Duration(seconds: 30),
+      connectTimeout: const Duration(seconds: 30),
+    ),
     enableLog: true,
   );
 
-  ChatCompleteText getResponse() => ChatCompleteText(
-    model: ChatModelFromValue(model: 'gpt-4'),
-    messages: [
-      {
-        "name": "url_extractor",
-        "schema": {
-          "type": "object",
-          "properties": {
-            "urls": {
-              "type": "array",
-              "description":
-                  "A collection of extracted URLs from the raw string.",
-              "items": {
-                "type": "string",
-                "description": "A URL extracted from the provided raw string.",
+  Future<String> getChatResponse(String message) async {
+    final response = ChatCompleteText(
+      model: Gpt4OChatModel(),
+      maxToken: 200,
+      messages: [
+        Messages(
+          role: Role.system,
+          content: 'You have to extract data from raw string.',
+        ).toJson(),
+        Messages(
+          role: Role.user,
+          content:
+              'The string I provide, contains: 1. userId with optional (categories), urls with optional category.',
+        ).toJson(),
+        Messages(role: Role.user, content: message).toJson(),
+      ],
+      responseFormat: ResponseFormat.jsonSchema(
+        jsonSchema: JsonSchema(
+          name: 'url extractor',
+          schema: {
+            'type': 'array',
+            'items': {
+              'type': 'object',
+              'properties': {
+                'userId': {'type': 'string'},
+                'url': {'type': 'string'},
+                'category': {'type': 'string'},
               },
+              'required': ['extractedData'],
             },
           },
-          "required": ["urls"],
-          "additionalProperties": false,
-        },
-        "strict": true,
-      },
-    ],
-  );
+        ),
+      ),
+    );
+    return response.messages.first['content'];
+  }
 }
